9a5c0b6d9f66593326de0702c149eb25ca40ce0c
Guillermo <guillermo.cuevas@axity.com>
2020-04-26 18:55:08 -0400

    Commit inicial

---
+++ src/main/java/com/sistema/ventas/superventas/SuperVentasApplication.java
@@ -0,0 +1,14 @@
+package com.sistema.ventas.superventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperVentasApplication.class, args);
+		System.out.println("HOLA MUNDO!!!");
+	}
+
+}
---
+++ src/test/java/com/sistema/ventas/superventas/SuperVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.ventas.superventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
